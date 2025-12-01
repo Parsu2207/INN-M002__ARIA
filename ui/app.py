1b6169bf2131d335336da0f0297b9a34aa69c151
Aditya Dhamanekar <adityadhamnekar123@gmail.com>
2025-12-01 17:51:51 +0530

    added correct spelling of PyQt5  library

--- ui/app.py
+++ ui/app.py
@@ -1,10 +1,12 @@
 import sys
-from pyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication
 from .main_window import MainWindow
 
+
 def run_desktop():
-  app=QApplication(sys.argv)
-  win=MainWindow()
-  win.show()
-  sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
   
+
