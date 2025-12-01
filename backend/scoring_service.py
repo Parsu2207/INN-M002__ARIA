341f1f950a641ebb5312e5186fde7f5acf4bbf23
Prasad Shinde <79320666+Parsu2207@users.noreply.github.com>
2025-12-01 12:57:13 +0530

    intial scoreSystem

--- backend/scoring_service.py
+++ backend/scoring_service.py
@@ -1,1 +1,14 @@
+from kafka import py5qt
 
+for score in range(len(json)):
+  if py5qt > 0.8 and py5qt < 1.0:
+    return "Critical"
+  elif py5qt < 0.79 and py5qt > 6.0:
+    return "High"
+  elif py5qt < 0.59 and py5qt > 4.0:
+    return "Medium"
+  elif py5qt < 0.4:
+    return "Low"
+  else:
+    return NULL
+  
