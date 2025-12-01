2ae52ba3137e5230c5fe72c3f56e47edd6544f15
Balekundrisufi <balekundrisufi@gmail.com>
2025-12-01 13:01:29 +0530

    Updated model logic
    basic idea

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,32 @@
+#backend/models.py
+from typing import List,Dist,Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+class Alert(BaseModel):
+  alert_id:str
+  timestamp:datetime
+  source:str
+  severity:str
+  event_type:dist[str,str]
+  raw:Dist
+  
+class ScoredAlert(Alert):
+  supervised_prob:float
+  anomaly_score:float
+  rule_boost:float
+  priority_bucket:str
+  top_features:List[str]
+
+class Incident(BaseModel):
+  incident_id:str
+  alerts:List[ScoredAlert]
+  created_at:datetime
+  entities:Dist[str,str]
+  priority_bucket:str
+  
+ 
+  
+  
+
 
